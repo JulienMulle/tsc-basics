f1f5da230fceddd3ccae76165cd242385cd3a0b5
Julien <julienmulle@gmail.com>
2022-06-14 13:50:34 +0200

    maj

--- challenge.ts
+++ challenge.ts
@@ -14,8 +14,8 @@ const prettyPrintWilder = (users: User[]) : void=> {
   
   const wilders : User [] = [];
   const user1 :User = { name: "Pierre", age: 23 };
-  const user2 = { name: "Paul", age: 32 };
-  const user3 :User= { name: "Jacques", age: 25 };
+  const user2 :User = { name: "Paul", age: 32 };
+  const user3 :User = { name: "Jacques", age: 25 };
   wilders.push(user1);
   wilders.push(user2);
   wilders.push(user3);
