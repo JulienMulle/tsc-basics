9638711274c93a8ee5dfa0a620c191048baa25ed
Julien <julienmulle@gmail.com>
2022-06-14 15:49:31 +0200

    POO typescript

---
+++ POO typescript1.ts
@@ -0,0 +1,28 @@
+class Person {
+    private name: string;
+    private age: number;
+
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    tellMyName() :string{
+        console.log(`I am ${this.name}`);
+        return this.name
+    };
+
+    tellMyAge() :number {
+        console.log(`I am ${this.age} years old`)
+        return this.age
+    }
+
+}
+
+const person1 = new Person ('John', 40 );
+const person2 = new Person('Mary', 35);
+person1.tellMyAge();
+person1.tellMyName();
+person2.tellMyAge();
+person2.tellMyName();
